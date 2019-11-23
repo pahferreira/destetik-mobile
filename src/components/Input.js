63bd271f6622ddb773492bcda4dea1c11a293c51
Pah Ferreira <pah.dsf@gmail.com>
2019-11-23 12:30:56 -0300

    implementing masked text

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,26 +5,62 @@
 
 import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { TextInputMask } from 'react-native-masked-text';
 import Colors from '../theme/Colors';
 
 type Props = {
   name: string,
   keyboardType?: any,
   safeEntry?: boolean,
+  onChangeText: Function,
+  value: string,
+  maskType?: string,
+  maskConfig?: {},
 };
 
 const Input = (props: Props) => {
-  const { name, keyboardType, safeEntry } = props;
-  return (
-    <View style={styles.container}>
-      <Text>{name}</Text>
+  const {
+    name,
+    keyboardType,
+    safeEntry,
+    onChangeText,
+    value,
+    maskType,
+    maskConfig,
+  } = props;
+
+  const _renderInput = () => {
+    if (maskType) {
+      return (
+        <TextInputMask
+          placeholder={name}
+          autoCapitalize="none"
+          type={maskType}
+          options={maskConfig}
+          value={value}
+          keyboardType={keyboardType ? keyboardType : 'default'}
+          style={styles.input}
+          secureTextEntry={safeEntry ? true : false}
+          onChangeText={text => onChangeText(text)}
+        />
+      );
+    }
+    return (
       <TextInput
         placeholder={name}
         autoCapitalize="none"
+        value={value}
         keyboardType={keyboardType ? keyboardType : 'default'}
         style={styles.input}
         secureTextEntry={safeEntry ? true : false}
+        onChangeText={text => onChangeText(text)}
       />
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <Text>{name}</Text>
+      {_renderInput()}
     </View>
   );
 };
